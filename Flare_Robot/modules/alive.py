15f602563419626efb3b97c4ce99327c72b95325
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-02-03 23:22:17 +0530

    Update alive.py

--- Flare_Robot/modules/alive.py
+++ Flare_Robot/modules/alive.py
@@ -9,21 +9,21 @@ from Flare_Robot.events import register
 from Flare_Robot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/6266d4d7ce030b8a7cf2d.jpg"
+PHOTO = "https://telegra.ph//file/8be8938f6a7f17df7d65c.jpg"
 
 
 @register(pattern=("/alive"))
 async def awake(event):
     TEXT = "**♡ I,m ғʟᴀʀᴇ ʀᴏʙᴏᴛ 愛** \n\n"
-    TEXT += f"**♡ I'm Working With sᴇxʏ Speed** \n\n"
+    TEXT += f"**♡ I'm Working With ultra flame Speed** \n\n"
     TEXT += f"**♡ ғʟᴀʀᴇ: LATEST Version** \n\n"
-    TEXT += f"**♡ My Creator: [ ᴀsᴛᴀ](http://t.me/Asta_silva02)** \n\n"
-    TEXT += f"**♡ ᴀɴʏ ɪssᴜᴇs ᴄᴏɴᴛᴀᴄᴛ ʜᴇʀᴇ @Freia_Support** \n\n"
+    TEXT += f"**♡ My Creator: [ ᴀsᴛᴀ](http://t.me/harshahero)** \n\n"
+    TEXT += f"**♡ ᴀɴʏ ɪssᴜᴇs ᴄᴏɴᴛᴀᴄᴛ ʜᴇʀᴇ @SenkuChat** \n\n"
     TEXT += "**♡ ᴛʜᴀɴᴋ ʏᴏᴜ ғᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ 💘💘💘**"
     BUTTON = [
         [
-            Button.url("📢 Updates", "https://t.me/Freia_Updates"),
-            Button.url("🚑 Support", "https://t.me/Freia_Support"),
+            Button.url("📢 Updates", "https://t.me/SENKUBOTS"),
+            Button.url("🚑 Support", "https://t.me/SENKUCHAT"),
         ]
     ]
     await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
